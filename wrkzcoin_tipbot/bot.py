622e990cd6d40cf5bd8b0028a4bf2ef1ac51bafb
Learner <linhdqtg@gmail.com>
2019-07-15 07:05:36 +0700

    Update

--- wrkzcoin_tipbot/bot.py
+++ wrkzcoin_tipbot/bot.py
@@ -4546,7 +4546,9 @@ def truncate(number, digits) -> float:
 @click.command()
 def main():
     #bot.loop.create_task(update_balance_wallets())
-    update_balance_wallets()
+    for coin in ENABLE_COIN:
+        print("Updating balances for "+coin)
+        await store.sql_update_balances(coin.upper())
     bot.run(config.discord.token, reconnect=True)
 
 
