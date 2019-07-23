f89e52687d822c59efbfc43a01af14180b5997f6
Learner <linhdqtg@gmail.com>
2019-07-23 12:07:50 +0700

    different check

--- wrkzcoin_tipbot/bot.py
+++ wrkzcoin_tipbot/bot.py
@@ -1829,7 +1829,7 @@ async def send(ctx, amount: str, CoinAddress: str):
                                'is invalid. (Syntax: .send <amount> <address> or .send <amount> <address.paymentid> )')
                 return
         originalCoinAddress = None
-        if len(CoinAddress) == get_intaddrlen(COIN_NAME) and valid_address != CoinAddress:
+        if hasattr(valid_address,'integrated_address') and valid_address != CoinAddress:
             originalCoinAddress = CoinAddress
             CoinAddress = valid_address['integrated_address']
         elif valid_address != CoinAddress:
