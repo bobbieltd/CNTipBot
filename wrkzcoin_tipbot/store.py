faa70c214ae4809590bb010af72ba4315a85cde5
Learner <linhdqtg@gmail.com>
2019-07-25 09:44:23 +0700

    fix NoneType

--- wrkzcoin_tipbot/store.py
+++ wrkzcoin_tipbot/store.py
@@ -133,7 +133,7 @@ async def sql_register_user(userID, coin: str = None):
                 cur.execute(sql, (str(userID), COIN_NAME))
                 result = cur.fetchone()
                 # recreate new address
-                if len(wallet['paymentid']) != 16 and len(wallet['paymentid']) != 64:
+                if paymentid not in wallet or (len(wallet['paymentid']) != 16 and len(wallet['paymentid']) != 64):
                     sql = """ DELETE FROM """+coin.lower()+"""_user_paymentid WHERE `user_id`=%s AND `coin_name` = %s LIMIT 1 """
                     cur.execute(sql, (str(userID), COIN_NAME))
                     result = None
