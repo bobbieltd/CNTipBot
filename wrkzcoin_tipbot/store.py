4cc6e72861c7c1082b7ab1c47f8ee8761a507c53
Learner <linhdqtg@gmail.com>
2019-07-26 11:43:21 +0700

    Convert to str

--- wrkzcoin_tipbot/store.py
+++ wrkzcoin_tipbot/store.py
@@ -75,7 +75,7 @@ async def sql_update_balances(coin: str = None):
         except Exception as e:
             traceback.print_exc(file=sys.stdout)
             height_start = 1
-        print('SQL: Updating get_transfers '+COIN_NAME+' start from '+height_start)
+        print('SQL: Updating get_transfers '+COIN_NAME+' start from '+str(height_start))
         # TODO take last height so no need to rescan all and scan from last height
         get_transfers = await wallet.get_transfers_xmr(COIN_NAME,height_start)
         if get_transfers is not None and len(get_transfers) >= 1 and coin_family == "XMR":
